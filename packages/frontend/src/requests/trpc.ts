47fadad6f891db74bd5fee29f748a79c5729b602
aiiiden <aiiiden.dev@gmail.com>
2025-08-26 17:17:01 +0900

    chore(frontend): config trpc client

--- packages/frontend/src/requests/trpc.ts
+++ packages/frontend/src/requests/trpc.ts
@@ -1,4 +1,19 @@
-import { createTRPCReact } from '@trpc/react-query';
+import {
+  createTRPCReact,
+  httpBatchLink,
+  createTRPCProxyClient,
+} from '@trpc/react-query';
 import type { AppRouter } from '../../../backend/src/index';
+import env from '@/lib/schema/env';
 
+// TRPC React Query
 export const trpc = createTRPCReact<AppRouter>();
+
+// TRPC Client
+export const trpcClient = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: `${env.data?.VITE_API_HOST}/trpc`,
+    }),
+  ],
+});
